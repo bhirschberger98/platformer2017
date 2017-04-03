2ef753635f6e4715881a9bc54cc8c929e41fba0e
bhirschberger98 <bretthirschberger@gmail.com>
2017-04-03 12:21:14 -0400

    added lives and points

--- Assets/Scripts/GM.cs
+++ Assets/Scripts/GM.cs
@@ -1,16 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class GM : MonoBehaviour {
+    public int _Lives = 3;
+    public int points;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Text lives_value;
+    public Text points_value;
+
+    public void Setlives(int newValue)
+    {
+        _Lives = newValue;
+        Debug.Log(_Lives);
+        lives_value.text = _Lives.ToString();
+    }
+
+    public int GetLives()
+    {
+        return _Lives;
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,47 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public int health = 100;
+    public float speed = 5;
+    public float jumpSpeed = 5;
+    public float deadzone=-3;
 
     new Rigidbody2D rigidbody;
+    GM _GM;
+    private Vector3 startingPosition;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        startingPosition = transform.position;
         rigidbody = GetComponent<Rigidbody2D>();
+        _GM = FindObjectOfType<GM>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate ()  {
+        //movement
         float x = Input.GetAxisRaw("Horizontal");
-        rigidbody.velocity = new Vector2(x, 0);
-	}
+        Vector2 v = rigidbody.velocity;
+        v.x = x * speed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            v.y = jumpSpeed;
+        }
+
+        rigidbody.velocity = v;
+
+        if (transform.position.y < deadzone)
+        {
+            
+            Getout();
+        }
+        
+        //rigidbody.AddForce (new Vector2(x * speed, 0));
+    }
+    public void Getout()
+    {
+        _GM.Setlives(_GM.GetLives()- 1);
+        transform.position = startingPosition;
+        Debug.Log("You're out");
+    }
 }
